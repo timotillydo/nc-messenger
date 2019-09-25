13bf851c47436aeaf3d12b9e26e5134b58f6b5b0
timotillydo <44656173+timotillydo@users.noreply.github.com>
2019-09-25 12:13:09 +0100

    save message via model and send back to client

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const insertMessage = require("./models/message-model");
 
 io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {
-    console.log("message: " + msg);
-    socket.emit("received message", msg);
+    console.log("message from fe: " + msg);
+    insertMessage(msg)
+      .then(storedMsg => socket.emit("received message", storedMsg))
+      .catch(err =>
+        socket.emit("message error", { errMsg: "something went wrong", err })
+      );
   });
 });
 
